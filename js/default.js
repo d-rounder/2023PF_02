c87692aa56b464859dc68042ac9187002d32ecf8
S-project92 <64456708+S-project92@users.noreply.github.com>
2023-08-23 15:42:50 +0900

    1.08

--- js/default.js
+++ js/default.js
@@ -133,47 +133,23 @@ $(document).ready(function(){
 
 
 //햄버거 버튼
-    var burger = $('.menu-trigger');
+var burger = $('.menu-trigger');
 
-    burger.each(function(index){
-      var $this = $(this);
-      
-      $this.on('click', function(e){
-        e.preventDefault();
-        $(this).toggleClass('active-' + (index+1));
-      })
-    });
+burger.each(function (index) {
+  var $this = $(this);
 
-    var h = 0;
+  $this.on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index + 1));
+  })
+});
 
-    $(burger).on("click", function () {
-      if (h == 0) {
-        $('.ham-con').animate({
-          right: '0',
-          opacity: 1
-        }, 500);
-        $(this).addClass('active-1');
-        h++;
-      } else if (h == 1) {
-        $('.ham-con').animate({
-          right: '-20%',
-          opacity: 0
-        }, 500);
-        $(this).removeClass('active-1');
-        h--;
-      }
-      $(window).scroll(function() {
-        sct = $(window).scrollTop();
-        if(sct>30){
-          $('.ham-con').css({
-            opacity: 0,
-            right :'-20%'
-          }, 500);
-          $(burger).removeClass('active-1');
-          h=0;
-        }
-      });
-    })
+var h = 0;
+
+$('#toggle').click(function() {
+  $(this).toggleClass('active');
+  $('#fullscreen').toggleClass('open');
+ });
 
 
 
